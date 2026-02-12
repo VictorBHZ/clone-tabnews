c184f0f16387874c9e31c0128bc954057257cf87
VictorBHZ <victorakimoto@gmail.com>
2026-02-12 19:24:41 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Obriagdo por me aguentar por tanto tempo. Se você me ama, dá uma risadinha. 🥰</h1>
+function Home() {
+  return (
+    <h1>
+      Obriagdo por me aguentar por tanto tempo. Se você me ama, dá uma
+      risadinha. 🥰
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
